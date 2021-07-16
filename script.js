3400bd9e2879531b78f2ad597447c88ccf9dcae4
tamunaadola <tamoadolashvili@gmail.com>
2021-07-16 18:54:40 +0400

    add function

---
+++ script.js
@@ -0,0 +1,9 @@
+// TASK 4
+let randomNumbers = [];
+for ( let i = 0; i < 100; i++){
+    let randomNumber = Math.round(Math.random()* 100);
+    randomNumbers.push(randomNumbers);
+}
+function getRandomNumber() {
+    return Math.round(Math.random()* 100);
+}
